c3fd3c8782e54ae822ad30b97288d35fc740c317
Kaelyn Crews <crewskaelyn@gmail.com>
2024-02-16 14:58:02 -0600

    fixing subclasses

--- src/pkg/CDRecordProduct.java
+++ src/pkg/CDRecordProduct.java
@@ -3,8 +3,8 @@ package pkg;
 
 public class CDRecordProduct extends MediaProduct {
 	
-	public CDRecordProduct(String title, double price, int year, Genre genre) {
-		super(title, price, year, genre);
+	public CDRecordProduct(String type, String title, double price, int year, Genre genre) {
+		super(type, title, price, year, genre);
 	}
 	
 	//Copy constructor
